0729dcef1641fab3b080394ca7fb6b5b88e75c3a
htech19 <141951250+htech19@users.noreply.github.com>
2026-03-26 04:46:58 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,32 +4,25 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CartProvider } from "@/contexts/CartContext";
-import Index from "./pages/Index";
-import PeliculasPage from "./pages/PeliculasPage";
+import Index from "./pages/Index"; // Agora o nome bate com o export default Index
 import LojaPage from "./pages/LojaPage";
-import ProductPage from "./pages/ProductPage";
-import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <CartProvider>
+    <CartProvider>
+      <TooltipProvider>
         <Toaster />
         <Sonner />
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
-            <Route path="/peliculas" element={<PeliculasPage />} />
             <Route path="/loja" element={<LojaPage />} />
-            <Route path="/loja/:id" element={<ProductPage />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
-      </CartProvider>
-    </TooltipProvider>
+      </TooltipProvider>
+    </CartProvider>
   </QueryClientProvider>
 );
 
